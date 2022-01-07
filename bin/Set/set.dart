f144cb53f8795078980c7e163fb811f2d56b2fe9
abi wardani <abiwardani@abis-MacBook-Pro.local>
2022-01-07 13:46:55 +0700

    add set

---
+++ bin/Set/set.dart
@@ -0,0 +1,15 @@
+void main() {
+  //cara 1
+  Set<int> nums = {1, 2, 3};
+
+  //cara 2
+  var num1 = {3, 2, 1};
+
+  print(nums);
+  print(num1);
+
+  //cara 3 deklarasi langsung
+  var myset = {'abi', 1, true};
+  print(myset);
+  ///Set adalah kumpulan data yang tidak bisa diulang
+}
---
+++ bin/Set/set_manipulation.dart
@@ -0,0 +1,10 @@
+void main() {
+  var angka = <int>{1, 2, 3};
+  angka.add(4);
+  print(angka);
+
+  angka.remove(1);
+  print(angka);
+
+  print(angka.length);
+}
