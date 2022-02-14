3e68d4f201fab82ccb61ae7031e8c60f2e2c756f
abi wardani <abiwardani@abis-MacBook-Pro.local>
2022-02-14 12:31:13 +0700

    edit directory

---
+++ variable/var.dart
@@ -0,0 +1,14 @@
+void main() {
+  //cara 1
+  String name;
+  name = 'abi';
+  //cara 2
+  String nama = 'abi';
+
+  //cara 3
+  var nama2 = 'abi';
+  print(name);
+  print(nama);
+  print(nama2);
+  //variable dart bisa dideklarasikan ulang
+}
