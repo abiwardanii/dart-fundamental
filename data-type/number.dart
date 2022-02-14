5fdc6b0f6e1bc17b5f283265b0db03897689d8b7
abi wardani <abiwardani@abis-MacBook-Pro.local>
2022-02-14 19:31:34 +0700

    update number

--- data-type/number.dart
+++ data-type/number.dart
@@ -1,13 +1,34 @@
 void main() {
+  /// int bilangan bulat
   int num1 = 10;
-  double num2 = 10.5;
-  num num3 = 10;
-  print(num1);
-  print(num2);
-  print(num3);
+  print(num1.runtimeType);
 
+  /// double bilangan desimal
+  double num2 = 10.5;
+  print(num2.runtimeType);
+  /// num bisa bilangan bulat dan desimal
+  
+  num num3 = 10;  
+  print(num3.runtimeType);
+
+  //method 
+
+  //floor = pembulatan kebawah
+  print(num2.floor());
+
+  //ceil = pembulatan keatas
+  print(num2.ceil());
+ 
+  //round = pembulatan
+  print(num2.round());
+  
+  //toDouble = mengubah ke double
+  print(num1.toDouble());
+
+  //toInt = mengubah ke int
+  print(num2.toInt());
 }
 
-/// int bilangan bulat
-/// double bilangan desimal
-/// num bisa bilangan bulat dan desimal
+
+
+
